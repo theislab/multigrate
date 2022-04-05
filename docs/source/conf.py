35d05737993e2c94553e09b03a67fd2c4e34f1f9
Anastasia Litinetskaya <alitinet@gmail.com>
2022-04-05 21:03:35 +0200

    updated docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,23 @@
 
 # -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import sys
+from pathlib import Path
+
+HERE = Path(__file__).parent
+sys.path[:0] = [str(HERE.parent), str(HERE / "extensions")]
+
+import multigrate
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'multigrate'
-copyright = '2022, Anastasia Litinetskaya, Mohammad Lotfollahi'
-author = 'Anastasia Litinetskaya, Mohammad Lotfollahi'
+project = "multigrate"
+copyright = "2022, Anastasia Litinetskaya, Mohammad Lotfollahi"
+author = "Anastasia Litinetskaya, Mohammad Lotfollahi"
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = "0.3.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,25 +31,91 @@ release = '0.3.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "nbsphinx",
+    "nbsphinx_link",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",  # needs to be after napoleon
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autosummary",
+    "scanpydoc.elegant_typehints",
+    "scanpydoc.definition_list_typed_field",
+    "scanpydoc.autosummary_generate_imported",
+    *[p.stem for p in (HERE / "extensions").glob("*.py")],
 ]
 
+# nbsphinx specific settings
+exclude_patterns = ["_build", "**.ipynb_checkpoints", "Thumbs.db", ".DS_Store"]
+nbsphinx_execute = "never"
+
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
 #
-html_theme = 'alabaster'
+source_suffix = [".rst", ".md"]
+
+# Generate the API documentation when building
+autosummary_generate = True
+autodoc_member_order = "bysource"
+napoleon_google_docstring = True  # for pytorch lightning
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_use_rtype = True  # having a separate entry generally helps readability
+napoleon_use_param = True
+napoleon_custom_sections = [("Params", "Parameters")]
+todo_include_todos = False
+numpydoc_show_class_members = False
+annotate_defaults = True  # scanpydoc option, look into why we need this
+myst_enable_extensions = [
+    "colon_fence",
+    "dollarmath",
+    "amsmath",
+]
+
+# The master toctree document.
+master_doc = "index"
+
+intersphinx_mapping = dict(
+    anndata=("https://anndata.readthedocs.io/en/stable/", None),
+    ipython=("https://ipython.readthedocs.io/en/stable/", None),
+    matplotlib=("https://matplotlib.org/", None),
+    numpy=("https://numpy.org/doc/stable/", None),
+    pandas=("https://pandas.pydata.org/docs/", None),
+    python=("https://docs.python.org/3", None),
+    scipy=("https://docs.scipy.org/doc/scipy/reference/", None),
+    sklearn=("https://scikit-learn.org/stable/", None),
+    torch=("https://pytorch.org/docs/master/", None),
+    scanpy=("https://scanpy.readthedocs.io/en/stable/", None),
+    pytorch_lightning=("https://pytorch-lightning.readthedocs.io/en/stable/", None),
+)
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = "default"
+pygments_dark_style = "native"
+
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = False
+
+html_theme = "sphinx_rtd_theme"
+html_title = "cpa-tools"
+
+html_show_sourcelink = False
+
+html_show_copyright = False
+
+display_version = True
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
+
+html_css_files = [
+    "css/custom.css",
+]
+
+html_favicon = "favicon.ico"
